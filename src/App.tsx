46d9dbbe660fbdd50331ad3a3a533d8848bf96ab
sajithlaldev <sajithlal65@gmail.com>
2022-06-01 19:56:13 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <div class='bg-green drop-shadow-2xl w-72 h-40'>
+      <p class="font-extrabold text-4xl py-20 bg-clip-text bg-gradient-to-r text-transparent from-indigo-500 via-purple-500 to-pink-500">My Todo App</p>
+
+      </div>
+
+    </div>
+  
+  );
+};
+
+export default App;
